31b2ae6a768171b36ee61e603d9368435809ffd0
Hadi <hadiyakhadija62@gmail.com>
2026-01-31 20:15:03 +0530

    ts errors fixed

--- src/types/custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "cookie-parser";
-declare module "passport";
-declare module "passport-google-oauth20";
-declare module "morgan";
-declare module "nodemailer";
-declare module "jsonwebtoken";
